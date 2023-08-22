c33d1975d2ae9cdb1193af9310e5d4be50991190
Simek <simek@mail.com>
2023-08-22 16:40:59 +0200

    Init structure

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Models.Products.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product.addProduct();
+        Product.addProduct();
+        Product.showProducts();
+    }
+}
